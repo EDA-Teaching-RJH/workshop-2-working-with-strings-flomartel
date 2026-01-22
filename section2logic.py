2a13e2d73b1620b664442215274c69aeda7a012b
flomartel <fjm25@kent.ac.uk>
2026-01-22 10:35:57 +0000

    version2

--- section2logic.py
+++ section2logic.py
@@ -1,2 +1,8 @@
 def main():
-    age = input(" How old are you? ")
+    age = input(" How old are you? ") #asks user for their name
+    if age < 18:
+        print("You are a child")
+    elif age >= 18:
+        print ("You are an adult")
+
+main()
