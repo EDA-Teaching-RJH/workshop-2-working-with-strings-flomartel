268fa65d95a35b07dd6f6d2f845355c02126f064
flomartel <fjm25@kent.ac.uk>
2026-01-22 09:46:16 +0000

    version4

--- partTwo.py
+++ partTwo.py
@@ -3,11 +3,13 @@ import math
 def main():
     A = int(input(" Input a value for A: "))
     B = int(input(" Input a value for B: "))
+    C = pythag(A,B)
 
 
 
 def pythag(A,B):
     C = math.sqrt(A**2 + B**2)
+    
 
 
 
