423063628a1f1fcc1117fc51ee1622a9688e3307
flomartel <fjm25@kent.ac.uk>
2026-01-22 09:51:17 +0000

    finalversion

--- partTwo.py
+++ partTwo.py
@@ -1,8 +1,8 @@
 import math
 
 def main():
-    A = int(input(" Input a value for A: "))
-    B = int(input(" Input a value for B: "))
+    A = int(input("Input a value for A: "))
+    B = int(input("Input a value for B: "))
     C = pythag(A,B)
 
 def pythag(A,B):
