66885e626256ec6133505b5aa237426eabc0667d
flomartel <fjm25@kent.ac.uk>
2026-01-22 12:00:51 +0000

    version 5

--- logicifelsestatement.py
+++ logicifelsestatement.py
@@ -4,8 +4,10 @@ secret_number = random.randint (1, 10)
 number = int(input("Guess a number between 1 and 10: "))
 if number == secret_number:
     print ("Well done, your guess is correct")
+
 elif number > secret_number:
     print ("Your value is too high")
+
 elif number < secret_number:
     print ("Your value is too low")
     
